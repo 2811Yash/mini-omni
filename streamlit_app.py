84b224ec59edba614f58fd905fc57603c6e511e8
2811Yash <118837106+2811Yash@users.noreply.github.com>
2024-10-05 23:52:13 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,59 @@
+import speech_recognition as sr
+import pyttsx3
 import streamlit as st
-from openai import OpenAI
+from langchain.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
+import google.generativeai as genai
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+load_dotenv()
+os.getenv("GOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+llm=ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
+# Initialize the recognizer
+recognizer = sr.Recognizer()
+voice=pyttsx3.init()
+close="close"
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+def omni(text):
+    template = "give the answer of this question {text}"
+    prompt = PromptTemplate(input_variables=["text"],
+                            template=template)
+    
+    chain=LLMChain(llm=llm,prompt=prompt,verbose=True)
+    result=chain(text)
+    return result
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+st.title("omni chatgpt-4o")
+# # st.write("on omni")
+if (st.button("📝  Give text input")):
+    que=st.text_input("type your question")
+    res1=omni(que)["text"]
+    st.write(res1)
+    if (st.button("🔊")):
+        voice.say(res1)
+    voice.runAndWait()
+st.write("OR")
+if (st.button("🎙️  Give the voice command")):
+    while 1:
+        with sr.Microphone() as source:
+            st.write("Speak something...")
+            recognizer.adjust_for_ambient_noise(source)
+            audio_data = recognizer.listen(source)
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            try:
+                text = recognizer.recognize_google(audio_data)
+                print("Converted Speech:")
+                st.write(text)
+                res=omni(text)["text"]
+                st.write(res)
+                voice.say(res)
+                voice.runAndWait()
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            except sr.UnknownValueError:
+                st.write("Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                st.write(f"Could not request results from Google Speech Recognition service; {e}")
